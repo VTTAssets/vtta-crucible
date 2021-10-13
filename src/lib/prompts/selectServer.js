2c0806608b3f1f6c585a9c6ee6ab13a1d4922fa2
Sebastian Will <vttassets@gmail.com>
2021-10-14 00:08:52 +0200

    Server deletion

--- src/lib/prompts/selectServer.js
+++ src/lib/prompts/selectServer.js
@@ -2,13 +2,12 @@ import env from "../env.js";
 import inquirer from "inquirer";
 
 import Servers from "../../servers/index.js";
-import CANCEL_PROMPT from "./index.js";
+
+const CANCEL_PROMPT = "<Cancel>";
 
 const selectServer = () => {
   return new Promise(async (resolve, reject) => {
     const servers = await Servers.list();
-
-    console.log(servers);
     const question = {
       type: "list",
       name: "server",
@@ -24,7 +23,7 @@ const selectServer = () => {
       .prompt([question])
       .then((answers) => {
         if (answers.server === CANCEL_PROMPT) reject(CANCEL_PROMPT);
-        else resolve(answer.server);
+        else resolve(answers.server);
       })
       .catch((error) => {
         console.log(error);
