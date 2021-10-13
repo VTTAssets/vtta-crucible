3b15d9a541f771ca926350b273d631959ca03de6
Sebastian Will <vttassets@gmail.com>
2021-10-14 00:12:24 +0200

    fix confirmation

--- src/lib/prompts/confirm.js
+++ src/lib/prompts/confirm.js
@@ -11,7 +11,7 @@ const confirm = (question) => {
           default: false,
         },
       ])
-      .then((answers) => resolve(answers.question))
+      .then((answers) => resolve(answers.choice))
       .catch((error) => resolve(false));
   });
 };
